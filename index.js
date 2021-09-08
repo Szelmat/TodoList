71bf86183e2bc29c2b6902fee1c3966049316df6
Máté Széles <szelessm@gmail.com>
2021-09-09 01:06:04 +0200

    add new function

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ function listTodo() {
     element.addEventListener('click', e => handleDelete(e));
   });
 }
+
+function newTodo(event) {
+    let name = prompt('Todo neve:');
+    let todo = new Todo(name);
+    todoList.push(todo);
+    listTodo();
+  }
+  
+  listTodo();
